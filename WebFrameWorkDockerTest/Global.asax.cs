c030065f8e054835f62366051ff5282f1d61241b
Bob He <cn49ak@hrsflow.com>
2022-10-18 19:02:56 +0800

    6 commit

--- WebFrameWorkDockerTest/Global.asax.cs
+++ WebFrameWorkDockerTest/Global.asax.cs
@@ -13,6 +13,7 @@ namespace WebFrameWorkDockerTest
     {
         void Application_Start(object sender, EventArgs e)
         {
+            
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
